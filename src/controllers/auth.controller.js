2f6c56ef090a02d015706d6114046c34c6dc5e24
nkwaaaa <nkwaaaa@gmail.com>
2023-06-19 18:36:08 -0300

    add cookie token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,32 @@
+import { createAccessToken } from "../libs/jwt.js"
 import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body
 
   try {
+    const passwordHash = await bcrypt.hash(password, 10)
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     })
 
     const userSaved = await newUser.save()
-    res.json(userSaved)
+    const token = await createAccessToken({ id: userSaved._id })
+
+    res.cookie("token", token)
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    })
   } catch (error) {
-    console.log(error)
+    res.status(500).json({ message: error.message })
   }
 }
 export const login = (req, res) => {
